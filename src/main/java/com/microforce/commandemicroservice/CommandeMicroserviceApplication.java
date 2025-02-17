72c7bd2912df104faa86ea0a69fc09318f355179
ibtissam el hani <ibtissamelhani36@gmail.com>
2025-02-17 10:18:39 +0100

    first commit

---
+++ src/main/java/com/microforce/commandemicroservice/CommandeMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.microforce.commandemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandeMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommandeMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microforce/commandemicroservice/CommandeMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microforce.commandemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandeMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
